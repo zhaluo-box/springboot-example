8bb6098d8265fb2ff1bbf310506f7b7b90f850d3
zl <zhaluobox@163.com>
2021-09-28 11:09:27 +0800

    init boot-base

---
+++ boot-base/src/test/java/com/example/boot/base/BootBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
