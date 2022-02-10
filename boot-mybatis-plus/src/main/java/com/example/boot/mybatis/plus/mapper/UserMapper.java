68d8636963629658d8815662808d0fb30d759772
zhaluobox <zhaluobox@163.com>
2022-02-10 23:28:07 +0800

    添加mybaits plus 的学习模块”

---
+++ boot-mybatis-plus/src/main/java/com/example/boot/mybatis/plus/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.example.boot.mybatis.plus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.boot.mybatis.plus.entiry.User;
+
+/**
+ * Created  on 2022/2/10 22:22:11
+ *
+ * @author zl
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ boot-mybatis-plus/src/test/java/com/example/boot/mybatis/plus/BootMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot.mybatis.plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
