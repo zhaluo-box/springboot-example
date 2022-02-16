1ee2e7328d6a8f0674d538e0d7b1675a8d7aab51
zl <zhaluobox@163.com>
2022-02-16 17:35:15 +0800

    add boot-flowable demo

---
+++ boot-flowable/src/main/java/com/example/boot/flowable/BootFlowableApplication.java
@@ -0,0 +1,14 @@
+package com.example.boot.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BootFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootFlowableApplication.class, args);
+    }
+
+}
---
+++ boot-flowable/src/test/java/com/example/boot/flowable/BootFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
